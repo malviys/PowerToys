64fe0e35e2bb1f5d72cd8ef64a12feeac5b7de6e
Sourabh Malviya <saurabhmalvia997@gmail.com>
2025-11-24 12:53:19 +0530

    Initial Commit

---
+++ PowerToys/PowerToysApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerToysApp.swift
+//  PowerToys
+//
+//  Created by Sourabh Malviya on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PowerToysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerToysTests/PowerToysTests.swift
@@ -0,0 +1,16 @@
+//
+//  PowerToysTests.swift
+//  PowerToysTests
+//
+//  Created by Sourabh Malviya on 24/11/25.
+//
+
+import Testing
+
+struct PowerToysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
